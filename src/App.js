e31150bf1f4af5436443f556a94ab1efe67ff836
Hardik Sharma <33150286+hardiks77@users.noreply.github.com>
2021-08-22 21:03:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Content from './Components/Content';
+import Header from './Components/Header';
+import "./App.scss";
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Content />
+        </div>
+    )
+}
+
+export default App;
